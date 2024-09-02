427d58545e05b693800c8ac573209a23f34b7c42
Khadija Janaliyeva <92391618+jxadica@users.noreply.github.com>
2024-09-02 17:25:09 +0400

    s

--- about/about.js
+++ about/about.js
@@ -1,18 +1,19 @@
 import { importFooter, importHeader,initializeButtonStyles } from "../app.js";
 document.querySelector("header").innerHTML = importHeader();
 document.querySelector("footer").innerHTML = importFooter();
-initializeButtonStyles();
-let portfolioImg = document.querySelectorAll(".component img");
-let portfolioOverlay = document.querySelectorAll(".portfolio-img-overlay");
+initializeButtonStyles();let portfolioComponents = document.querySelectorAll(".component");
 
-portfolioImg.forEach(function (img, index) {
-    img.addEventListener("mouseover", function () {
+portfolioComponents.forEach(function (component, index) {
+    let img = component.querySelector("img");
+    let overlay = component.querySelector(".portfolio-img-overlay");
+
+    component.addEventListener("mouseover", function () {
         img.style.filter = 'brightness(50%)'; 
-        portfolioOverlay[index].style.display = 'flex'; 
+        overlay.style.display = 'flex'; 
     });
 
-    img.addEventListener("mouseleave", function () {
+    component.addEventListener("mouseleave", function () {
         img.style.filter = 'brightness(100%)'; 
-        portfolioOverlay[index].style.display = 'none'; 
+        overlay.style.display = 'none'; 
     });
 });
